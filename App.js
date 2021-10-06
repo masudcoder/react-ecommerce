f91065dc077e6ac38da6c998f4b77ce84825f1f1
masud <enggmasud1983@gmail.com>
2021-10-06 23:36:14 +0900

    Add files via upload

---
+++ App.js
@@ -0,0 +1,51 @@
+import React from "react";
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import Home from './Pages/Home'
+import About from './Pages/About'
+import Contact from './Pages/Contact'
+
+
+export default function App() {
+  return (
+    <div className="App">
+      <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact Us</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+    </div>
+  );
+}
